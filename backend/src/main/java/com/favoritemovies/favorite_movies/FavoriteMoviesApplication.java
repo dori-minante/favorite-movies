d75d854f205d7ee78419e67c4bcdf96d43aeb8f8
Dori Minante <dorim.dev@gmail.com>
2025-01-15 23:07:14 -0300

    first commit

---
+++ backend/src/main/java/com/favoritemovies/favorite_movies/FavoriteMoviesApplication.java
@@ -0,0 +1,13 @@
+package com.favoritemovies.favorite_movies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FavoriteMoviesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FavoriteMoviesApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/favoritemovies/favorite_movies/FavoriteMoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.favoritemovies.favorite_movies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FavoriteMoviesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
